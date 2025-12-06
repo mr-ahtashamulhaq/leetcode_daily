727eacee9fb7d3cf7025126486e146f5d59bede3
mr-ahtashamulhaq <mr.ahtashamulhaq@gmail.com>
2025-12-07 03:02:40 +0500

    update

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+print(  )
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        key_dict = {}
+        answer = []
+        for i in range(len(nums)):
+            for j in range(i+1 ,  len(nums)):
+                remaining = -1 * (nums[i] + nums[j])
+                if remaining in key_dict:
+                    answer.append([nums[i] , nums[j] , remaining])
+                else:
+                
+                    key_dict[i] = key_dict.get(i,0)
+
+
+        return answer
+        
+obj = Solution()
