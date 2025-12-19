f4a37061c1f99253c3bfe580e5507efdf169e74d
mr-ahtashamulhaq <mr.ahtashamulhaq@gmail.com>
2025-12-20 00:12:11 +0500

    Create Day-007-contains-duplicate.py

---
+++ Day-007-contains-duplicate/Day-007-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = {}
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            seen[nums[i]] = 0
+
+        return False
