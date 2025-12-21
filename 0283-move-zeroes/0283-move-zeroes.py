7e424ce22f11e34eed9ab64eb872c0323203fdd0
Mr. Ahtasham Ul Haq <mr.ahtashamulhaq@gmail.com>
2025-12-21 23:32:36 +0500

    Time: 9 ms (38.27%), Space: 13.4 MB (92.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,20 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        if len(nums) == 1:
-            return
+        n = len(nums)
         i = 0
-        j = 0
+        if n ==1:
+            return
 
-        while i < len(nums) and j < len(nums):
-            while i < len(nums) and nums[i] != 0:
-                i+=1
-            j = i+1 
-            while j < len(nums) and nums[j] == 0:
-                j+=1
-            if j < len(nums):
-                nums[i],nums[j]=nums[j],nums[i]
+        while(i<n):
+            if nums[i] == 0:
+                break
             i+=1
-            # j+=1
-
+        if i == n:
+            return
         
+        j = i+1
+        while(j<n):
+            if nums[j] != 0:
+                nums[j] , nums[i] = nums[i] , nums[j]
+                i +=1
+            j+=1
