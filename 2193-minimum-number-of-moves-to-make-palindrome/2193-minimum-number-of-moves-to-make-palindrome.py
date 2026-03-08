b5e95042920320170fe5d91fa0fd8dd079f1f863
Mr. Ahtasham Ul Haq <mr.ahtashamulhaq@gmail.com>
2026-03-08 18:41:56 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.py
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.py
@@ -3,26 +3,25 @@ class Solution:
         s = list(s)
         ans = 0
         lt = 0
-        rt = len(s) - 1
-
+        rt = len(s) -1
         while lt < rt:
 
             i = rt
             j = rt
 
-            while i > lt and s[i] != s[lt]:
-                i -= 1
-
-            if i == lt:
+            while i > lt and s[i] != s[lt]: #Move left pointer forward until found same element
+                i-=1
+            
+            if i == lt :
                 s[i], s[i+1] = s[i+1], s[i]
                 ans += 1
+            
             else:
-                while i < j:
+                while i  < j:
                     s[i], s[i+1] = s[i+1], s[i]
-                    ans += 1
                     i += 1
+                    ans += 1
 
                 lt += 1
                 rt -= 1
-
         return ans
