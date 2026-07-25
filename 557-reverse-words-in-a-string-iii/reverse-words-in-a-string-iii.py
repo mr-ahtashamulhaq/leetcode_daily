32d82395466eaedb81d8429ad1c753cb236d7c4f
Mr. Ahtasham Ul Haq <mr.ahtashamulhaq@gmail.com>
2026-07-25 21:24:49 +0500

    Delete Duplicate Files

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        l = s.split()
-        for i in range(len(l)):
-            l[i] = l[i][::-1]
-            
-        return " ".join(l)
