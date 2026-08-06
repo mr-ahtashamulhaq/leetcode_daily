2c3cf3df2d36d8ba3e5f4186ebf43be3985f9546
Mr. Ahtasham Ul Haq <mr.ahtashamulhaq@gmail.com>
2026-08-06 22:22:23 +0500

    Delete Duplicates

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minBitFlips(self, start: int, goal: int) -> int:
-        ans = start ^ goal
-        count =0
-        for i in range(0,32):
-            if (ans>>i) & 1 ==1:
-                count +=1
-        return count
