e933d4a8d9354b08da5973156d46ba388c49cf7d
Mr. Ahtasham Ul Haq <mr.ahtashamulhaq@gmail.com>
2026-08-14 23:30:18 +0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (69.74%) - LeetSync

---
+++ 899-binary-gap/binary-gap.py
@@ -0,0 +1,14 @@
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        b=bin(n)[2:]
+        if b.count('1')<=1:
+            return 0
+        c=0
+        m=0
+        for i in b:
+            if i=='0':
+                c+=1
+            elif i=='1':
+                m=max(m,c+1)
+                c=0
+        return m
